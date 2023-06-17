03b3752a8fcdb53e147a0e067fc8605c86b1d8cb
Akhmad Fauzan Prayogi <akhmadfauzanprayogi@gmail.com>
2023-06-17 14:17:55 +0700

    style(login, register): cleanup code after revamp

--- app/src/main/java/com/hikizan/hikigram/presentation/membership/LoginActivity.kt
+++ app/src/main/java/com/hikizan/hikigram/presentation/membership/LoginActivity.kt
@@ -4,18 +4,12 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Patterns
-import android.widget.Toast
 import androidx.core.content.ContextCompat
-import com.hikizan.hikigram.R
 import com.hikizan.hikigram.base.HikizanActivityBase
 import com.hikizan.hikigram.databinding.ActivityLoginBinding
 import com.hikizan.hikigram.presentation.MainActivity
-import com.hikizan.hikigram.utils.emailFieldValidation
-import com.hikizan.hikigram.utils.ext.isValidPassword
 import com.hikizan.hikigram.utils.hikizanEmailStream
 import com.hikizan.hikigram.utils.hikizanPasswordStream
-import com.hikizan.hikigram.utils.passwordFieldVlaidation
 import com.hikizan.hikigram.utils.showEmailExistAlert
 import com.hikizan.hikigram.utils.showPasswordExistAlert
 import io.reactivex.Observable
@@ -73,9 +67,6 @@ class LoginActivity : HikizanActivityBase<ActivityLoginBinding>() {
     @SuppressLint("CheckResult")
     override fun initObservers() {
         binding?.apply {
-            emailFieldValidation(etEmail, tilEmail)
-            passwordFieldVlaidation(etPassword, tilPassword)
-
             val emailStream = hikizanEmailStream(etEmail)
             emailStream.subscribe {
                 showEmailExistAlert(this@LoginActivity, tilEmail, it)
